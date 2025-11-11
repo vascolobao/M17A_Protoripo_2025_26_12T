c3f22f665c03c9296c5a01767dfb85753ec2538a
ESEN <ESEN@LAPTOP-HN5TSDC7>
2025-11-11 09:20:56 +0000

    Adicionar arquivos de projeto.

---
+++ M17A_Protoripo_2025_26_12T/F_Principal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace M17A_Protoripo_2025_26_12T
+{
+    public partial class F_Principal : Form
+    {
+        public F_Principal()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
